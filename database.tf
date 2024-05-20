0faf67558492f975815c723167917e02e3000df2
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-19 22:58:29 -0400

    Update database.tf

--- database.tf
+++ database.tf
@@ -22,3 +22,29 @@ resource "azurerm_mysql_server" "mysqlapps" {
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
+
+resource "azurerm_resource_group" "mysqlapps" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "sqlservercreation" {
+  for_each = {for mssqlserver in local.juliosqlserver_app_list : "${mssqlserver.name}" => mssqlsesrver}
+ 
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
