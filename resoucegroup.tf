89bf60e2f74b832004e3e6602abf01797aeb84f9
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-20 22:57:14 -0400

    Update resoucegroup.tf

--- resoucegroup.tf
+++ resoucegroup.tf
@@ -1,9 +1,3 @@
-# Define variables
-variable "adminstrator_login_password" {
-  description = "The password for the administrator login."
-  default     = "default_password"
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "juliosqlserver" {
   name     = "example-resources"
