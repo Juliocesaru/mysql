de3b11ec7fb86e3bf468840c1531abb34d9bc376
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-20 23:50:38 -0400

    Update resoucegroup.tf

--- resoucegroup.tf
+++ resoucegroup.tf
@@ -1,5 +1,5 @@
 # Create a resource group
-resource "azurerm_resource_group" "juliosqlserver"{
+resource "azurerm_resource_group" "juliosqlserver" {
   name     = "juliosqlserver"
   location = "Central canada"
 }
