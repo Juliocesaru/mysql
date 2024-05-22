1d82e5e0928a7d5151c6ae6f1851a4fc2f29dac7
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-21 22:47:26 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals{
   myjuliosql_app=[for f in fileset("${path.module}/config", "[^_]*.yaml") : yamldecode(file("${path.module}/config/${f}"))]
   myjuliosql_app_list = flatten([
-    for app in local.juliosqlserver_app : [
+    for app in local.myjuliosql_app : [
       for mysqlapps in try(app.listofmsqlapp, []) :{
         name = mysqlapps.name
 
